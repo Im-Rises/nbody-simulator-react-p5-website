dedb12e95cf257a12513fcf34d3820802f51ed40
Im-Rises <quentin-morel88@hotmail.com>
2023-05-17 11:54:00 -0400

    Corrected loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
 import NbodySimulator from 'nbody-simulator-react-p5';
 import {useRef} from 'react';
 
 const App = () => {
 	const divRef = useRef(null);
+	const [isLoaded, setIsLoaded] = React.useState(false);
+
+	useEffect(() => {
+		if (divRef.current) {
+			setIsLoaded(true);
+		}
+	}, [divRef]);
+
 	return (
 		<div className='App' ref={divRef}>
-			<NbodySimulator parentRef={divRef}/>
+			{isLoaded
+				? (<NbodySimulator parentRef={divRef}/>)
+				: (<div>Is loaded</div>)
+			}
 		</div>
 	);
 };
